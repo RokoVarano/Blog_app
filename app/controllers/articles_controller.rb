4872dadda55b2c2befe6d08ca2ca663fb220ae9f
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-02-09 16:47:34 -0500

    add edit method and button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  # R
   def show
     @article = Article.find(params[:id])
   end
 
-  # C
   def new
     @article = Article.new
   end
@@ -23,8 +21,23 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private
-    def article_params
-      params.require(:article).permit(:title, :body)
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
