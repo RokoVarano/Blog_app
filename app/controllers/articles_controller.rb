4baf9f4f493833a859da19b05d66a613955dbe13
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-02-09 18:32:48 -0300

    Create New Article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,28 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  # R
   def show
     @article = Article.find(params[:id])
   end
+
+  # C
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
